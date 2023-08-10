e54b1d014b4360fd9d70eac2f1cd4f342934790d
Salma <141500222+salmasewilam@users.noreply.github.com>
2023-08-10 17:26:18 +0300

    Add files via upload

---
+++ Python/TheLetter_Is_Vowel.py
@@ -0,0 +1,6 @@
+vowel=['a','e','i','o','u','A','E','I','O','U']
+letter=input("please enter the letter  ")
+if letter in vowel:
+    print("the letter is a vowel")
+else:
+    print("the letter is a consonant")
