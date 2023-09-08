d1e5dd4a1a8f0a301415b8590efc54eec5502e40
salmasewilam <salmasewlam2@gmail.com>
2023-09-08 15:26:27 +0300

    c++

---
+++ c++/ASCII_Table.cpp
@@ -0,0 +1,17 @@
+//Create a table for AscII code
+
+#include <iostream>
+
+int main()
+{
+    std:: cout<<"ASCII code Table\n";
+    std:: cout<<"----------------\n";
+    std:: cout<<"| Char | ASCII |\n";
+    std:: cout<<"----------------\n";
+    for (unsigned char i=0;i<128;i++)
+    {
+        std:: cout<<"|  "<<'i'<<"  |\n";
+    }
+
+    return 0;
+}
